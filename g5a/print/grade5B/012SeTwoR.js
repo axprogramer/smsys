2c16b1cf0ccd3287763a2eb0c28e61e763f23cdf
axprogramer <105089786+axprogramer@users.noreply.github.com>
2022-07-06 10:31:28 +0700

    update

--- g5a/print/grade5B/012SeTwoR.js
+++ g5a/print/grade5B/012SeTwoR.js
@@ -267,8 +267,8 @@ function myPrintSeTwoR(){
     td2.innerHTML = sex;
     td3.innerHTML = grade;
     td4.innerHTML = my2Sa;
-    td5.innerHTML = total;
-    td6.innerHTML = finall;
+    td5.innerHTML = parseFloat(total).toFixed(2);
+    td6.innerHTML = parseFloat(finall).toFixed(2);
     td7.innerHTML = my2SeRa;
   
   
