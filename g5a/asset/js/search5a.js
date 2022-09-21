2796a96c47d204af174577f85d435b1db5256dc1
axprogramer <105089786+axprogramer@users.noreply.github.com>
2022-09-21 10:33:32 +0700

    update

--- g5a/asset/js/search5a.js
+++ g5a/asset/js/search5a.js
@@ -134,3 +134,219 @@ $(document).ready(function ()
 
 })
 
+//December
+$(document).ready(function ()
+{
+    var Sf = "F";
+    $("input[type=button]").click(function ()
+    {
+        getOccurance(Sf);
+        var rowCount = $("#tbody4 td").closest("tr").length;
+        var show = document.getElementById("TotalSsDec");
+        show.innerHTML = rowCount;
+        var show2 = document.getElementById("TotalSs2Dec");
+        show2.innerHTML = rowCount;
+
+
+
+    });
+
+    function getOccurance(word)
+    {
+        var noOfOccurance = 0;
+
+        $("#tbody4 td:gt(2)").each(function (ind, obj)
+        {
+            if (word == $.trim($(obj).text())) noOfOccurance++;
+
+        });
+
+        var show = document.getElementById("TotalSFDec");
+        show.innerHTML = noOfOccurance;
+        var show3 = document.getElementById("TotalSF2Dec");
+        show3.innerHTML = noOfOccurance;
+
+    }
+
+})
+
+//January
+$(document).ready(function ()
+{
+    var Sf = "F";
+    $("input[type=button]").click(function ()
+    {
+        getOccurance(Sf);
+        var rowCount = $("#tbody5 td").closest("tr").length;
+        var show = document.getElementById("TotalSsJan");
+        show.innerHTML = rowCount;
+        var show2 = document.getElementById("TotalSs2Jan");
+        show2.innerHTML = rowCount;
+
+
+
+    });
+
+    function getOccurance(word)
+    {
+        var noOfOccurance = 0;
+
+        $("#tbody5 td:gt(2)").each(function (ind, obj)
+        {
+            if (word == $.trim($(obj).text())) noOfOccurance++;
+
+        });
+
+        var show = document.getElementById("TotalSFJan");
+        show.innerHTML = noOfOccurance;
+        var show3 = document.getElementById("TotalSF2Jan");
+        show3.innerHTML = noOfOccurance;
+
+    }
+
+})
+
+
+//February
+$(document).ready(function ()
+{
+    var Sf = "F";
+    $("input[type=button]").click(function ()
+    {
+        getOccurance(Sf);
+        var rowCount = $("#tbody6 td").closest("tr").length;
+        var show = document.getElementById("TotalSsFeb");
+        show.innerHTML = rowCount;
+        var show2 = document.getElementById("TotalSs2Feb");
+        show2.innerHTML = rowCount;
+
+
+
+    });
+
+    function getOccurance(word)
+    {
+        var noOfOccurance = 0;
+
+        $("#tbody6 td:gt(2)").each(function (ind, obj)
+        {
+            if (word == $.trim($(obj).text())) noOfOccurance++;
+
+        });
+
+        var show = document.getElementById("TotalSFFeb");
+        show.innerHTML = noOfOccurance;
+        var show3 = document.getElementById("TotalSF2Feb");
+        show3.innerHTML = noOfOccurance;
+
+    }
+
+})
+
+//1st Semester
+$(document).ready(function ()
+{
+    var Sf = "F";
+    $("input[type=button]").click(function ()
+    {
+        getOccurance(Sf);
+        var rowCount = $("#tbody7 td").closest("tr").length;
+        var show = document.getElementById("TotalSsFirSem");
+        show.innerHTML = rowCount;
+        var show2 = document.getElementById("TotalSs2FirSem");
+        show2.innerHTML = rowCount;
+
+
+
+    });
+
+    function getOccurance(word)
+    {
+        var noOfOccurance = 0;
+
+        $("#tbody7 td:gt(2)").each(function (ind, obj)
+        {
+            if (word == $.trim($(obj).text())) noOfOccurance++;
+
+        });
+
+        var show = document.getElementById("TotalSFFirSem");
+        show.innerHTML = noOfOccurance;
+        var show3 = document.getElementById("TotalSF2FirSem");
+        show3.innerHTML = noOfOccurance;
+
+    }
+
+})
+
+//1st Semester Result
+$(document).ready(function ()
+{
+    var Sf = "F";
+    $("input[type=button]").click(function ()
+    {
+        getOccurance(Sf);
+        var rowCount = $("#tbody8 td").closest("tr").length;
+        var show = document.getElementById("TotalSsFirSemRe");
+        show.innerHTML = rowCount;
+        var show2 = document.getElementById("TotalSs2FirSemRe");
+        show2.innerHTML = rowCount;
+
+
+
+    });
+
+    function getOccurance(word)
+    {
+        var noOfOccurance = 0;
+
+        $("#tbody8 td:gt(2)").each(function (ind, obj)
+        {
+            if (word == $.trim($(obj).text())) noOfOccurance++;
+
+        });
+
+        var show = document.getElementById("TotalSFFirSemRe");
+        show.innerHTML = noOfOccurance;
+        var show3 = document.getElementById("TotalSF2FirSemRe");
+        show3.innerHTML = noOfOccurance;
+
+    }
+
+})
+
+//1st Semester Result
+$(document).ready(function ()
+{
+    var Sf = "F";
+    $("input[type=button]").click(function ()
+    {
+        getOccurance(Sf);
+        var rowCount = $("#tbody9 td").closest("tr").length;
+        var show = document.getElementById("TotalSsMarch");
+        show.innerHTML = rowCount;
+        var show2 = document.getElementById("TotalSs2March");
+        show2.innerHTML = rowCount;
+
+
+
+    });
+
+    function getOccurance(word)
+    {
+        var noOfOccurance = 0;
+
+        $("#tbody9 td:gt(2)").each(function (ind, obj)
+        {
+            if (word == $.trim($(obj).text())) noOfOccurance++;
+
+        });
+
+        var show = document.getElementById("TotalSFMarch");
+        show.innerHTML = noOfOccurance;
+        var show3 = document.getElementById("TotalSF2March");
+        show3.innerHTML = noOfOccurance;
+
+    }
+
+})
