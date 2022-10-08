dd4e4b728dca3be094be6b8132083a62e96444f3
axprogramer <105089786+axprogramer@users.noreply.github.com>
2022-10-08 09:28:29 +0700

    update

--- g5a/asset/js/search5b.js
+++ g5a/asset/js/search5b.js
@@ -45,6 +45,53 @@ function tableSearchKh(){
     }
 
 }
+function tableSearchEngB(){
+    let input, filter, table, tr, td, textValue;
+    input = document.getElementById("myInputEn");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("tbody1");
+    tr = table.getElementsByTagName("tr");
+
+    for (let i = 0; i < tr.length; i++){
+       
+        td = tr[i].getElementsByTagName("td")[1];
+
+        if(td){
+            textValue = td.textContent || td.innerText;
+            if(textValue.toUpperCase().indexOf(filter) > -1){
+                tr[i].style.display = "";
+            }
+            else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+
+}
+
+function tableSearchKhB(){
+    let input, filter, table, tr, td, textValue;
+    input = document.getElementById("myInputKh");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("tbody1");
+    tr = table.getElementsByTagName("tr");
+
+    for (let i = 0; i < tr.length; i++){
+       
+        td = tr[i].getElementsByTagName("td")[2];
+
+        if(td){
+            textValue = td.textContent || td.innerText;
+            if(textValue.toUpperCase().indexOf(filter) > -1){
+                tr[i].style.display = "";
+            }
+            else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+
+}
 
 function clickEng(){
     var inputKh = document.getElementById("myInputKh");
